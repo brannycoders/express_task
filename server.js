44fb86e3d74d9f18e80a85bcd7600cb2f965c41a
Nathaniel <nathanielbn@gmail.com>
2024-12-03 22:41:39 +0100

    Add .gitignore to exclude node_modules

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+
+
+
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Express');
+});
+
+app.get('/about', (req, res) => {
+    res.send('This is the About page.');
+});
+
+app.get('/greet/:Nathaniel', (req, res) => {
+    const { name } = req.params;
+    res.send(`Hello, ${Nathaniel}!`);
+});
+
+app.get('/api/users', (req, res) => {
+    const users = [
+        { id: 1, name: 'User 1' },
+        { id: 2, name: 'User 2' },
+    ];
+    res.json(users);
+});
+
+
+app.use((req, res) => {
+    res.status(404).send('404 - Page Not Found');
+});
+
+
+
+
+
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
